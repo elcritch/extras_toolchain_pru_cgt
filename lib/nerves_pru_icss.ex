d20a416e9ee242cb637e5b7fac3ad2175ee03359
Jaremy Creechley <creechley@gmail.com>
2018-01-19 17:19:42 -0700

    Initial commit

---
+++ lib/nerves_pru_icss.ex
@@ -0,0 +1,18 @@
+defmodule NervesPruIcss do
+  @moduledoc """
+  Documentation for NervesPruIcss.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesPruIcss.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_pru_icss_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesPruIcssTest do
+  use ExUnit.Case
+  doctest NervesPruIcss
+
+  test "greets the world" do
+    assert NervesPruIcss.hello() == :world
+  end
+end
