6652fe6a9d5f8da71fc9da26d8a7901259f4d285
Jaremy Creechley <creechley@gmail.com>
2018-01-25 22:58:24 -0700

    testing creating a toolchain

--- mix.exs
+++ mix.exs
@@ -6,25 +6,59 @@ defmodule NervesPruIcss.MixProject do
       app: :nerves_pru_icss,
       version: "0.1.0",
       elixir: "~> 1.6",
+      compilers: [:nerves_package | Mix.compilers()],
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: "A wrapper for compiler TI's PRU C/C++ Compiler",
-      deps: deps()
+      deps: deps(),
+      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
+  def nerves_package do
     [
-      extra_applications: [:logger]
+      type: :extras,
+      platform: Nerves.Toolchain.CTNG,
+      platform_config: [
+      ],
+      target_tuple: :arm_unknown_linux_gnueabihf,
+      artifact_sites: [
+        {:github_releases, "elcritch/nerves_pru_icss"}
+      ],
+      checksum: package_files()
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
+  def application do
+    []
+  end
+
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:nerves, "~> 0.9", runtime: false},
     ]
   end
+
+
+  defp package do
+    [
+      maintainers: ["Jaremy Creechley"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{
+        "Github" =>
+        "https://github.com/elcritch/nerves_pru_icss/#{@app}"
+      }
+    ]
+  end
+
+  defp package_files do
+    [
+      "README.md",
+      "LICENSE",
+      "mix.exs",
+      "VERSION"
+    ]
+  end
+
 end
