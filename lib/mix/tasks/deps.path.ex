cc56c96c5b57ae696b2ee6ab409314f40bc791a9
Jaremy Creechley <creechley@gmail.com>
2018-01-31 18:25:15 -0700

    deps path task... ?

--- lib/mix/tasks/deps.path.ex
+++ lib/mix/tasks/deps.path.ex
@@ -1,7 +1,19 @@
-defmodule Mix.Tasks.Build.Env do
+defmodule Mix.Tasks.Nerves.Path.Dep do
   use Mix.Task
 
-  @shortdoc "Simply runs the Hello.say/0 command."
+  @shortdoc "Print the path for a given dependency "
+  def run([dep]) do
+    value = Map.get(Mix.Project.deps_paths, String.to_atom(dep)) || ''
+    IO.puts "#{dep |> String.upcase}='#{value}'"
+  end
+  def run(_) do
+  end
+end
+
+defmodule Mix.Tasks.Nerves.Path.Artifact do
+  use Mix.Task
+
+  @shortdoc "Print the path for a given nerves artifact "
   def run([dep]) do
     value = Map.get(Mix.Project.deps_paths, String.to_atom(dep)) || ''
     IO.puts "#{dep |> String.upcase}='#{value}'"
