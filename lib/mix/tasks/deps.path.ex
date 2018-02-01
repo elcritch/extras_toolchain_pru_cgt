9568f04bdda78812f8e2176709dec67216aa4eb3
Jaremy Creechley <creechley@gmail.com>
2018-01-31 18:48:24 -0700

    deps path task... ?

--- lib/mix/tasks/deps.path.ex
+++ lib/mix/tasks/deps.path.ex
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.Nerves.Path.Dep do
   @shortdoc "Print the path for a given dependency "
   def run([dep]) do
     value = Map.get(Mix.Project.deps_paths, String.to_atom(dep)) || ''
-    IO.puts "#{dep |> String.upcase}='#{value}'"
+    IO.puts "#{dep |> String.upcase}=\"#{value}\""
   end
   def run(_) do
   end
