fd952a150bbd1f5b53e3bac7fed3a493ddb28afe
Jaremy Creechley <creechley@gmail.com>
2018-01-31 18:04:15 -0700

    deps path task... ?

--- lib/mix/tasks/deps.path.ex
+++ lib/mix/tasks/deps.path.ex
@@ -4,12 +4,12 @@ defmodule Mix.Tasks.Build.Env do
   @shortdoc "Simply runs the Hello.say/0 command."
   def run(arg) do
     # calling our Hello.say() function from earlier
+    IO.puts "build:env: #{inspect arg}"
     case arg do
       [dep] ->
-        IO.puts Map.get(Mix.Project.deps_paths, arg)
-        :ok
+        IO.puts Map.get(Mix.Project.deps_paths, dep)
       _ ->
-        :error
+        nil
     end
   end
 end
