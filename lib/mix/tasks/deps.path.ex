2a83de4f2b2444d6d3fc871a944e68d765397008
Jaremy Creechley <creechley@gmail.com>
2018-01-31 18:44:45 -0700

    deps path task... ?

--- lib/mix/tasks/deps.path.ex
+++ lib/mix/tasks/deps.path.ex
@@ -15,7 +15,8 @@ defmodule Mix.Tasks.Nerves.Path.Artifact do
 
   @shortdoc "Print the path for a given nerves artifact "
   def run([dep]) do
-    value = Map.get(Mix.Project.deps_paths, String.to_atom(dep)) || ''
+    dep_paths = Nerves.Env.packages() |> Enum.map(fn d -> {d.app, d.path} end) |> Map.new
+    value = Map.get(deps_paths, String.to_atom(dep)) || ''
     IO.puts "#{dep |> String.upcase}='#{value}'"
   end
   def run(_) do
