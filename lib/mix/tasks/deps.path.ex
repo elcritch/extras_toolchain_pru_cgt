035bd479cae6222dc7d8be4fc9dcf71826555d53
Jaremy Creechley <creechley@gmail.com>
2018-01-31 22:43:34 -0700

    deps path task... ?

--- lib/mix/tasks/deps.path.ex
+++ lib/mix/tasks/deps.path.ex
@@ -3,7 +3,6 @@ defmodule Mix.Tasks.Nerves.Path.Dep do
 
   @shortdoc "Print the path for a given dependency "
   def run([dep]) do
-    IO.puts "HELLOOO"
     value = Map.get(Mix.Project.deps_paths, String.to_atom(dep)) || ''
     IO.puts "#{value}"
   end
@@ -16,23 +15,19 @@ defmodule Mix.Tasks.Nerves.Path.Artifact do
 
   @shortdoc "Print the path for a given nerves artifact "
   def run([dep]) do
-    IO.puts "WORLDDDD"
 
-    Mix.Project.get!()
-    deps = Mix.Dep.loaded([])
+    Nerves.Env.start
+    Nerves.Env.ensure_loaded(Mix.Project.config[:app])
 
-    # Nerves.Env.start
-    # Nerves.Env.ensure_loaded(Mix.Project.config[:app])
+    dep_paths = Nerves.Env.packages() |> Enum.map(fn d -> {d.app, d} end) |> Map.new
 
-    # dep_paths = Nerves.Env.packages() |> Enum.map(fn d -> {d.app, d} end) |> Map.new
-
-    # case Map.get(dep_paths, String.to_atom(dep)) do
-    #   nil ->
-    #     IO.puts ""
-    #   nerves_package ->
-    #     # IO.puts "#{inspect nerves_package}"
-    #     IO.puts "#{Nerves.Artifact.dir nerves_package}"
-    # end
+    case Map.get(dep_paths, String.to_atom(dep)) do
+      nil ->
+        IO.puts ""
+      nerves_package ->
+        # IO.puts "#{inspect nerves_package}"
+        IO.puts "#{Nerves.Artifact.dir nerves_package}"
+    end
   end
 
   def run(_) do
