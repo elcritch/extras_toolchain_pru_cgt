017ef229454f9883b54f9b79ba01dabebc80b413
Jaremy Creechley <creechley@gmail.com>
2018-02-24 20:08:57 -0700

    updates

--- lib/pru_cgt_platform.ex
+++ lib/pru_cgt_platform.ex
@@ -7,11 +7,39 @@ defmodule NervesPruIcss.Toolchain do
   import Mix.Nerves.Utils
 
   # @callback bootstrap(Nerves.Package.t()) :: :ok | {:error, error :: term}
-  def bootstrap(pkg) do
+  # def bootstrap(pkg) do
 
-    System.put_env("NERVES_PRU", "ECHO")
-    IO.puts "ECHO!!!!"
+  #   System.put_env("NERVES_PRU", "ECHO")
+  #   IO.puts "ECHO!!!!"
 
+  #   :ok
+  # end
+
+  @doc """
+  Called as the last step of bootstrapping the Nerves env.
+  """
+  def bootstrap(%{config: config} = pkg) do
+    IO.puts "extras:bootsrapping: pkg: #{inspect pkg}"
+
+    if Keyword.has_key?(config[:toolchain_extras], :boostrap_override) do
+      boot_func = config[:toolchain_extras][:boostrap_override]
+      boot_func.(pkg)
+    else
+      default_bootstrap(pkg)
+    end
+  end
+
+  defp default_bootstrap(%{path: path} = pkg) do
+    artifact_path =
+      Artifact.base_dir()
+      |> Path.join(Artifact.name(pkg))
+
+    env_var = pkg.config[:toolchain_extras][:env_var]
+
+    IO.puts "extras:bootsrapping: build_path: #{inspect artifact_path}"
+    IO.puts "extras:bootsrapping: put_env: envvar: #{inspect env_var}"
+
+    System.put_env(env_var, artifact_path)
     :ok
   end
 
