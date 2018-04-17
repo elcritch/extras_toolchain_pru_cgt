c0d989805f0d714a0268f9436c61c00b5548ba1e
Jaremy Creechley <creechley@gmail.com>
2018-04-16 22:41:48 -0600

    Update version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule NervesPruIcss.MixProject do
     [
       app: @app,
       name: "PruCGT",
-      version: "2.2.1",
+      version: "2.2.2",
       elixir: "~> 1.4",
       compilers: [:nerves_package] ++ Mix.compilers(),
       nerves_package: nerves_package(),
