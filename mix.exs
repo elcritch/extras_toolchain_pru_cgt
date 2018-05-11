506e559c027a464a43091b1e470207d60f57323c
Jaremy Creechley <creechley@gmail.com>
2018-05-11 17:19:10 -0600

    Update compile steps

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule NervesPruIcss.MixProject do
       description: "A wrapper for compiler TI's PRU C/C++ Compiler",
       deps: deps(),
       package: package(),
-      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"], loadconfig: [&bootstrap/1]]
+      # aliases: ["deps.precompile": ["nerves.env", "deps.precompile"], loadconfig: [&bootstrap/1]]
+      aliases: [loadconfig: [&bootstrap/1]],
     ]
   end
 
